ff8dde06699eb0f91e60b301a07070e6b2cf4bdf
Barricature <irisxia2016@gmail.com>
2023-04-06 21:37:06 -0400

    Added Description

--- ui-design/secondary_view/index.js
+++ ui-design/secondary_view/index.js
@@ -11,12 +11,18 @@ const imageScroll = document.getElementById('image-scroll-bar');
 for(let i = 0; i < 10; i++) {
     const img = document.createElement('div');
     img.className = ('image-container');
+
     const pic = document.createElement('img');
     pic["src"] = i % 2 === 0 ? './sample_image.jpg' : './sample_image_2.jpg';
     pic.className = 'pic';
+
     const comment = document.createElement('p');
-    comment.textContent = 'Comment'
-    comment.className = 'comment'
+    comment.textContent = 'Comment';
+    comment.className = 'comment';
+
+    const description = document.createElement('p');
+    description.textContent = 'Description';
+    description.className = 'description';
     function toggle() {
         let opened = false;
         const popup = document.createElement('div');
@@ -24,25 +30,29 @@ for(let i = 0; i < 10; i++) {
         popup.innerHTML = '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>';
         function open() {
             if(!opened) {
-                comment.appendChild(popup);
+                description.appendChild(popup);
                 opened = true;
             } else {
-                comment.removeChild(popup);  
+                description.removeChild(popup);  
                 opened = false;
             } 
         }
         return open;    
     }
-    comment.addEventListener('click', toggle());
+    description.addEventListener('click', toggle());
+
     const like = document.createElement('p');
     like.textContent = 'LIKE'
     like.className = 'like'
+
     const author = document.createElement('p');
     author.textContent = 'Iris Xia'
     author.className = 'author'
+
     img.appendChild(pic);
     img.appendChild(comment);
     img.appendChild(like);
+    img.appendChild(description);
     img.appendChild(author);
     imageScroll.appendChild(img);
 }
