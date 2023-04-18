4a991aa2849090e45e68eca0ef35fd085adfb56b
mstian2024 <71884987+mstian2024@users.noreply.github.com>
2023-04-18 02:36:04 -0400

    added functionality to update description
    now it will update the text in the description box, like always needs DB implementation afterwards

--- docs/ui-design/files/Profile/profile.js
+++ docs/ui-design/files/Profile/profile.js
@@ -2,7 +2,13 @@ const dark = document.getElementById('themeDark');
 const light = document.getElementById('themeLight');
 const content = document.getElementById('content');
 const privatemode = document.getElementById('privatemode');
+const desc = document.getElementById('profDesc');
+const updateDesc = document.getElementById('saveDesc');
+const newDesc = document.getElementById('newDesc');
+const themElems = ['content','modal-header','modal-body','newDesc','modal-dialog']
 
+//initilize it to false, but we'll update it according to the db later
+var privateCheck = false;
 dark.addEventListener("click", (event) => {
     //TODO LINK IT TO THE POUCHDB
     //remove the previous lists 
@@ -19,6 +25,15 @@ privatemode.addEventListener("click", (event) =>{
     //console.log(privateCheck);
     //TODO LINK IT TO DB
 });
+
+updateDesc.addEventListener("click", (event)=>{
+    //console.log("ok!");
+    desc.innerHTML = newDesc.value;
+    newDesc.value = "Your new description!"
+    //TODO LINK IT TO DB
+});
+
+//TODO RETRIEVE DB PART.
 
 function updateTheme(theme){
     for(let elem in themElems){
