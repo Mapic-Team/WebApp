5dd5533a497db5019da444b3d952a200b87fb444
mstian2024 <71884987+mstian2024@users.noreply.github.com>
2023-04-18 02:14:07 -0400

    added functionality to the theme buttons

--- docs/ui-design/files/Profile/profile.js
+++ docs/ui-design/files/Profile/profile.js
@@ -1,1 +1,14 @@
+const dark = document.getElementById('themeDark');
+const light = document.getElementById('themeLight');
+const content = document.getElementById('content');
 
+dark.addEventListener("click", (event) => {
+    //TODO LINK IT TO THE POUCHDB
+    //remove the previous lists 
+    updateTheme('darkTheme');
+});
+
+light.addEventListener("click", (event)=>{
+    //TODO LINK IT TO THE POUCHDB
+    updateTheme('whiteTheme');
+});
