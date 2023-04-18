df2290b075ce4ecfb9c4af0dcef688139c6cd6cc
mstian2024 <71884987+mstian2024@users.noreply.github.com>
2023-04-18 02:33:27 -0400

    added privatemode checker and update function
    The update function should've been included but I didn't save it before the commit

--- docs/ui-design/files/Profile/profile.js
+++ docs/ui-design/files/Profile/profile.js
@@ -1,6 +1,7 @@
 const dark = document.getElementById('themeDark');
 const light = document.getElementById('themeLight');
 const content = document.getElementById('content');
+const privatemode = document.getElementById('privatemode');
 
 dark.addEventListener("click", (event) => {
     //TODO LINK IT TO THE POUCHDB
@@ -12,3 +13,17 @@ light.addEventListener("click", (event)=>{
     //TODO LINK IT TO THE POUCHDB
     updateTheme('whiteTheme');
 });
+
+privatemode.addEventListener("click", (event) =>{
+    privateCheck = privateCheck?false:true;
+    //console.log(privateCheck);
+    //TODO LINK IT TO DB
+});
+
+function updateTheme(theme){
+    for(let elem in themElems){
+        console.log(themElems[elem]);
+        document.getElementById(themElems[elem]).setAttribute("class", themElems[elem]);
+        document.getElementById(themElems[elem]).classList.add(theme);
+    }
+}
