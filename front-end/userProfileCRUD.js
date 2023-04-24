4d853e863c7c1a89855f80458a2d16a92d04820e
Elvira Xiong <47922282+rxiong19@users.noreply.github.com>
2023-04-23 21:30:09 -0400

    user database set up finished

--- front-end/userProfileCRUD.js
+++ front-end/userProfileCRUD.js
@@ -1,10 +1,8 @@
 import PouchDB from "pouchdb";
 import * as http from "http";
 import * as url from "url";
-import { md5 } from "blueimp-md5";
-
+import md5 from "md5";
 let users = {};
-
 
 const headerFields = { "Content-Type": "text/html" };
 
@@ -24,28 +22,41 @@ let res = await db.allDocs({
   attachments: true,
 });
 
+const userProfile={
+  _id:"",
+  userName:"", 
+  password: "",
+  profileDescription: "I am new to MaPic!",
+  setting: {isVisible:true}
+
+}
+
 async function reload() {
   users = res !== null ? res : {};
 }
 
 function userExists(name) {
-  return name in users;
+  return md5(name) in users;
 }
 
-async function createUser(response, name) {
-  if (name === undefined) {
+async function createUser(response, name, password) {
+  if (name === undefined || userExists(name)) {
     // 400 - Bad Request
     response.writeHead(400, headerFields);
     response.write("<h1>User Name Required</h1>");
     response.end();
   } else {
     reload();
-    users[name] = 0;
-    console.log("creating...");
-    console.log(users);
+    const id = md5(name).toString();
+    let new_user = userProfile;
+    new_user["_id"] = id;
+    new_user["userName"] = name;
+    new_user["password"] = password;
+    users[id] = new_user;
+  
     await db.post(users);
     response.writeHead(200, headerFields);
-    response.write(`<h1>User ${name} Created</h1>`);
+    response.write(`<h1>User ${name} and ${md5(name)}Created</h1>`);
     response.end();
   }
 }
@@ -55,7 +66,7 @@ async function readUser(response, name) {
     reload();
     console.log(users);
     response.writeHead(200, headerFields);
-    response.write(`<h1>User ${name} = ${users[name]}</h1>`);
+    response.write(`<h1>User ${name} and ${users[md5(name)]}</h1>`);
     response.end();
   } else {
     // 404 - Not Found
@@ -65,19 +76,28 @@ async function readUser(response, name) {
   }
 }
 
-async function updateUser(response, name) {
-  if (userExists(name)) {
+async function updateUser(response, old_name, new_name, new_password,new_profileDescription) {
+  if (userExists(old_name)) {
     response.writeHead(200, headerFields);
     reload();
-    users[name] += 1;
+    let id = md5(old_name).toLocaleString();
+    delete users[id];
+    id = md5(new_name).toLocaleString();
+    let update_profile =userProfile;
+    update_profile["_id"]=id;
+    update_profile["userName"]=new_name;
+    update_profile["password"]=new_password;
+    update_profile["profileDescription"]=new_profileDescription;
+    users[id]=update_profile;
+    //need to do separate fucntion for setting
     await db.post(users);
     console.log(users);
-    response.write(`<h1>User ${name} Updated</h1>`);
+    response.write(`<h1>User ${old_name} Updated</h1>`);
     response.end();
   } else {
     // 404 - Not Found
     response.writeHead(404, headerFields);
-    response.write(`<h1>User ${name} Not Found</h1>`);
+    response.write(`<h1>User ${old_name} and \n ${md5(old_name)} Not Found</h1>`);
     response.end();
   }
 }
@@ -86,7 +106,7 @@ async function deleteUser(response, name) {
   if (userExists(name)) {
     response.writeHead(200, headerFields);
     reload();
-    delete users[name];
+    delete users[md5(name)];
     await db.post(users);
     console.log(users);
     response.write(`<h1>User ${name} Deleted</h1>`);
@@ -99,9 +119,9 @@ async function deleteUser(response, name) {
   }
 }
 
-function dumpCounters(response) {
+function dumpUsers(response) {
   response.writeHead(200, headerFields);
-  response.write("<h1>Counters</h1>");
+  response.write("<h1>Users</h1>");
   response.write("<ul>");
   for (const [key, value] of Object.entries(users)) {
     response.write(`<li>${key} = ${value}</li>`);
@@ -114,15 +134,16 @@ async function basicServer(request, response) {
   const options = url.parse(request.url, true).query;
 
   if (request.url.startsWith("/users/create")) {
-    createUser(response, options.name);
+    createUser(response, options.name, options.password);
   } else if (request.url.startsWith("/users/read")) {
     readUser(response, options.name);
   } else if (request.url.startsWith("/users/update")) {
-    updateUser(response, options.name);
+    updateUser(response, options.old_name, options.new_name,
+       options.new_password, options.new_profileDescription);
   } else if (request.url.startsWith("/users/delete")) {
     deleteUser(response, options.name);
   } else {
-    dumpCounters(response);
+    dumpUsers(response);
   }
 }
 
@@ -130,3 +151,9 @@ async function basicServer(request, response) {
 http.createServer(basicServer).listen(3000, () => {
   console.log("Server started on port 3000");
 });
+
+/**
+ * http://localhost:3000/users/create?name=JohnDoe&password=123
+ * http://localhost:3000/users/update?old_name=JohnDoe&new_name=JaneDoe&password=123
+ * http://localhost:3000/users/read?name=JaneDoe
+ */
