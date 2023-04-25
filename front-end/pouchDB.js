246ab0a15673f1838a3e80f024b1b231051a1f96
Barricature <irisxia2016@gmail.com>
2023-04-24 21:07:02 -0400

    set template, created dbs

--- front-end/pouchDB.js
+++ front-end/pouchDB.js
@@ -1,2 +1,68 @@
-//This file should handle all reading and writing realted to the database
-//It should export functions that all three pages (mapic, profile, and secondary_view, and read)
+// This file should handle all reading and writing realted to the database
+// It should export functions that all three pages (mapic, profile, and secondary_view, and read)
+// The functions need not be async for now
+
+var PouchDB = require('pouchdb');
+
+const pictureDB = new PouchDB('pictureDB');
+const userDB = new PouchDB('userDB');
+
+/**
+ * 
+ * @param {String} userName 
+ * @param {String} password
+ * @returns {void}
+ */
+function createUser(userName, password) {
+    user = {
+        userName: userName,
+        password: password
+    }
+    db.put(user, function(err, res) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('User Created')
+        }
+    });
+}
+
+function readUser() {
+    return null;
+}
+
+function updateUser() {
+    return null;
+}
+
+function deleteUser() {
+    return null;
+}
+
+
+// functions for secondary_view
+
+function increaseLike(userName) {
+    return null;
+}
+
+function addComment(picId) {
+    return null;
+}
+
+function readDescription(picId) {
+    return null;
+}
+
+function getUser(picId) {
+    return null
+}
+
+
+// Some functions needs to be exported, 
+// others like readUser and updateUser which act as helper functions remain private.
+
+exports.increaseLike = increaseLike;
+exports.addComment = addComment;
+exports.readDescription = readDescription;
+exports.getUser = getUser;
