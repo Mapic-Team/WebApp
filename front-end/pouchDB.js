a5c9c0fedfa92987225eadb90981f8dc44de4804
rxiong19 <rxiong@umass.edu>
2023-04-25 01:16:21 -0400

    roughly finished pictureDB crud and sceondary view crud

--- front-end/pouchDB.js
+++ front-end/pouchDB.js
@@ -4,6 +4,15 @@
 
 const PouchDB = require("pouchdb");
 const md5 = require("md5");
+const ReverseMd5 = require("reverse-md5");
+const rev = ReverseMd5({
+  lettersUpper: true,
+  lettersLower: true,
+  numbers: true,
+  special: false,
+  whitespace: true,
+  maxLen: 20,
+});
 
 const pictureDB = new PouchDB("pictureDB");
 const userDB = new PouchDB("userDB");
@@ -28,13 +37,13 @@ function getImgBase64(path) {
  * @returns {void}
  */
 function createUser(userName, password) {
-  user = {
-    userId: md5(userName),
+  let user = {
+    _id: md5(userName),
     userName: userName,
     password: password,
     userPicture: getImgBase64("front-end/resources/default_user_picture.png"),
     userDescription: "I am new to Mapic!",
-    userSetting: { isVisible: true },
+    userSetting: { isVisible: true, isDarkMode: false },
   };
   userDB.put(user, function (err, res) {
     if (err) {
@@ -44,35 +53,176 @@ function createUser(userName, password) {
     }
   });
 }
-
 function readUser(userName) {
-  return null;
+  return userDB.get(md5(userName));
 }
 
-function updateUser() {
-  return null;
+function updateUser(
+  oldUserName,
+  newUserName,
+  password,
+  path,
+  description,
+  userSetting
+) {
+  db.get(md5(oldUserName)).then(function (doc) {
+    // Update the document
+    doc._id = md5(newUserName);
+    doc.userName = newUserName;
+    doc.password = password;
+    doc.userPicture = getImgBase64(path);
+    doc.userDescription = description;
+    doc.userSetting = userSetting;
+
+    // Save the updated document
+    userDB.put(doc);
+  });
 }
 
-function deleteUser() {
-  return null;
+function deleteUser(userName) {
+  userDB.get(md5(userName)).then(function (doc) {
+    userDB.remove(doc);
+  });
+}
+
+//functions for pictureDB
+/**
+ *
+ * @param {String} userName
+ * @param {String} path
+ * @param {String} description
+ * @returns {void}
+ */
+function createPicture(userName, imgBase, description) {
+  let today = new Date();
+  let pic = {
+    _id: md5(imgBase) + md5(userName),
+    picBase64: imgBase,
+    picDescription: description,
+    picLike: 0,
+    picComment: [],
+    picDate: `${today.getFullYear()}/${
+      today.getMonth() + 1
+    }/${today.getDate()} ${today.getHours()}:${today.getMinutes()}`,
+  };
+  pictureDB.put(pic, function (err, res) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Picture Created");
+    }
+  });
+}
+
+function readPicture(userName, imgBase) {
+  return pictureDB.get(md5(imgBase) + md5(userName));
+}
+
+function updatePicture(userName, imgBase, description) {
+  pictureDB.get(md5(imgBase) + md5(userName)).then(function (doc) {
+    // Only allow to update the description of the picture
+    doc.picDescription = description;
+    pictureDB.put(doc);
+  });
+}
+
+function deletePicture(userName, imgBase) {
+  pictureDB.get(md5(imgBase) + md5(userName)).then(function (doc) {
+    return pictureDB.remove(doc);
+  });
 }
 
 // functions for secondary_view
 
-function increaseLike(userName) {
-  return null;
+/**
+ * This function increase the likes of a specific picture in the database by one.
+ * @param {Int} picId
+ * @returns {void}
+ */
+function increaseLike(picId) {
+  pictureDB.get(picId).then(function (doc) {
+    doc.picLike += 1;
+    pictureDB.put(doc);
+  });
 }
 
-function addComment(picId) {
-  return null;
+/**
+ * This function decrease the likes of a specific picture in the database by one.
+ * @param {Int} picId
+ * @returns {void}
+ */
+function decreaseLike(picId) {
+  pictureDB.get(picId).then(function (doc) {
+    doc.picLike -= 1;
+    pictureDB.put(doc);
+  });
 }
 
-function readDescription(picId) {
-  return null;
+/**
+ * Get the time field from exif of a specific picture, return the time as a string
+ * In the format "yyyy/mm/dd 00:00"
+ * @param {*} picId
+ * @returns {String}
+ */
+function getPicTime(picId) {
+  pictureDB.get(picId).then(function (doc) {
+    return doc.picDate;
+  });
 }
 
+/**
+ * Get all comments of a picture, return an array of comment objects in the form
+ * {commentString: “”, string
+	commentTime: "yyyy/mm/dd 00:00" string,
+	commentBy: int (userId)}
+ * @param {*} picId 
+ * @return {Array} returns an array of comment objects
+ */
+function getComments(picId) {
+  pictureDB.get(picId).then(function (doc) {
+    return doc.picComment;
+  });
+}
+/**
+ * This function add a new comment by a user to a picture
+ * @param {String} picId
+ * @param {String} comment the new comment that is being added
+ * @param {Int} userId
+ * @returns {void}
+ */
+function addComment(picId, comment, userId) {
+  let today = new Date();
+  const comment_profile = {
+    commentString: comment,
+    commentTime: `${today.getFullYear()}/${
+      today.getMonth() + 1
+    }/${today.getDate()} ${today.getHours()}:${today.getMinutes()}`,
+    commentBy: userId,
+  };
+  pictureDB.get(picId).then(function (doc) {
+    doc.comment.append(comment_profile);
+    pictureDB.put(doc);
+  });
+}
+
+/**
+ * Get the description field of a specific picture
+ * @param {*} picId
+ * @returns
+ */
+function getDescription(picId) {
+  pictureDB.get(picId).then(function (doc) {
+    return doc.picDescription;
+  });
+}
+
+/**
+ * Get the the user id of the owner of a picture
+ * @param {*} picId
+ * @returns {String}
+ */
 function getUser(picId) {
-  return null;
+  return rev(picId.slice(picId / 2));
 }
 
 // Some functions needs to be exported,
