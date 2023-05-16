3515ac3bfbb13893c35cabb0c30db86d3df31dc9
Max Tian <mstian@umass.edu>
2023-05-16 17:37:58 -0400

    added rest of the CRUD calls

--- client/CRUD.js
+++ client/CRUD.js
@@ -53,7 +53,36 @@ class crud{
        }
    }
    
-   async getBestPic(){
+   async createPicture(picture){
+    try{
+        const response = await fetch(`/createUser`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body:JSON.stringify({picture:picture})
+        });
+        const data = await response.json();
+        return data;
+    }
+    catch(err){
+        return false;
+    }
+   }
+
+   async deletePicture(picId){
+        try{
+        const response = await fetch(`/createUser?picId${picId}`, {
+            method: 'DELETE',
+            headers: {'Content-Type': 'application/json'},
+        });
+        const data = await response.json();
+        return true;
+        }
+        catch(err){
+            return false;
+        }
+   }
+
+   async getMostLikedPic(){
        try{
            const response = await fetch(`/getMostLikedPic`, {
                method: 'GET',
@@ -75,7 +104,7 @@ class crud{
                body:JSON.stringify({userName:name,setting:settings})
            });
            const data = await response.json();
-           return true;
+           return data;
        }
        catch(err){
            return false;
@@ -84,20 +113,91 @@ class crud{
 
    async changeProfilePic(name, profilePic){
     try{
-        const response = await fetch(`/updateSettings`, {
+        const response = await fetch(`/updateProfilePicture`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body:JSON.stringify({userName:name,profilePicture: profilePic})
         });
         const data = await response.json();
-        return true;
+        return data;
     }
     catch(err){
         return false;
     } 
    }
 
+   async changeDescription(name, description){
+    try{
+        const response = await fetch(`/updateDescription`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body:JSON.stringify({userName:name,description: description})
+        });
+        const data = await response.json();
+        return data;
+    }
+    catch(err){
+        return false;
+    } 
    }
+
+   async changeLikeBy(picId,change){
+    try{
+        const response = await fetch(`/changeLikeBy`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body:JSON.stringify({picId:picId,change: change})
+        });
+        const data = await response.json();
+        return data;
+    }
+    catch(err){
+        return false;
+    } 
+   }
+   async addComment(picId, comment){
+    try{
+        const response = await fetch(`/addComment`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body:JSON.stringify({picId:picId,comment: comment})
+        });
+        const data = await response.json();
+        return data;
+    }
+    catch(err){
+        return false;
+    } 
+   }
+
+   async getTrending(){
+    try{
+        const response = await fetch(`/getTrending`, {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'},
+        });
+        const data = await response.json();
+        return data;
+    }
+    catch(err){
+        return false;
+    } 
+   }
+
+   async getTenMostCommonTags(){
+    try{
+        const response = await fetch(`/getTenMostCommonTags`, {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'},
+        });
+        const data = await response.json();
+        return data;
+    }
+    catch(err){
+        return false;
+    } 
+   }
+}
    
    const mapicCrud = new crud()
    export {mapicCrud}
