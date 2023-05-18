33160e45fe035746537109956348db428be48347
Max Tian <mstian@umass.edu>
2023-05-18 00:20:38 -0400

    added comments describing the code in login and profile

--- client/Profile/login.js
+++ client/Profile/login.js
@@ -9,7 +9,7 @@ const logPass = document.getElementById("logPass");
 const signNam = document.getElementById("signNam");
 const signPass = document.getElementById("signPass");
 
-
+//This is how the taabs work 
 document.getElementById("pills-home-tab").addEventListener("click",(event)=>{
     signNam.value="";
     signPass.value= "";
@@ -22,17 +22,9 @@ document.getElementById("pills-profile-tab").addEventListener("click",(event)=>{
     document.getElementById("logwarning").innerHTML = "";
 });
 
-/*function updateTheme(theme){
-    for(let elem in themElems){
-        console.log(themElems[elem]);
-        document.getElementById(themElems[elem]).setAttribute("class", themElems[elem]);
-        document.getElementById(themElems[elem]).classList.add(theme);
-    }
-}*/
-
+//this is the signup and login button functionality
 document.getElementById("signup").addEventListener("click" ,async (event)=>{
     if(await isValidSignUp()){
-        console.log("workedd");
         await mapicCrud.createUser(signNam.value,signPass.value);
         localStorage.setItem("user", signNam.value);
         location.href = "profile.html"
@@ -50,21 +42,21 @@ document.getElementById("login").addEventListener("click" ,async (event)=>{
  */
 async function isValidLogin(){
     let exists = "";
-    console.log(exists.password);
+    //we check if the either field is empty, is they are then prevent login
     if(isEmpty(logUser.value)||isEmpty(logPass.value)){
         document.getElementById("logwarning").innerHTML = "One of the Values is empty!";
         return false;
     }
     try{
         //we try to get the value from database
-        exists = await mapicCrud.readUser(logUser.value);
-        console.log(exists);
+         exists = await mapicCrud.readUser(logUser.value);
 
     }
     catch(err){
         //if we get an error it doesn't exist
         return false;
     }   
+    //we check if the user exists and the password is correct if not prevent login
     if(!exists.success || exists.data.password != logPass.value){
         document.getElementById("logwarning").innerHTML = "Incorrect password and username combo!";
         return false;
@@ -77,23 +69,22 @@ async function isValidLogin(){
  */
 async function isValidSignUp(){
     let exists = "";
+    //if either field is empty we give out a warning and prevent signuo
     if(isEmpty(signNam.value)||isEmpty(signPass.value)){
         document.getElementById("warning").innerHTML = "One of the Values is empty!";
         return false;
     }
     //check if we already have it in the database
         //we try to get the value from database
-    //console.log(signNam.value)
     exists = await mapicCrud.readUser(signNam.value);
-    //console.log(exists.success)
+    //if we can get a user that means we can't use that username so prevent sign in
      if(exists.success){
         document.getElementById("warning").innerHTML = "User already exists!";
         return false;
     }
     return true;
 }
-
+//we see if the selected string is empty or not
 function isEmpty(str) {
-    //console.log(!str.trim().length)
     return !str.trim().length;
 }
